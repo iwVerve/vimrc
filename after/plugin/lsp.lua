72d52fdaf895ded3bc03dbcaf46a5c72c8027400
iwVerve <thedolphiner@gmail.com>
2024-01-07 22:45:58 +0100

    Cleanup tabs

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,14 +3,14 @@ local lsp = require("lsp-zero")
 lsp.preset("recommend")
 
 lsp.set_preferences({
-	sign_icons = { }
+    sign_icons = { }
 })
 
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
+    local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
@@ -20,8 +20,8 @@ end)
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-	ensure_installed = {},
-	handlers = {
-		lsp.default_setup,
-	},
+    ensure_installed = {},
+    handlers = {
+        lsp.default_setup,
+    },
 })
--- lua/iwverve/packer.lua
+++ lua/iwverve/packer.lua
@@ -1,55 +1,55 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	-- Telescope
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-	-- Theme
-	use {
-		'maxmx03/dracula.nvim',
-		as = 'dracula',
-		config = function()
-			vim.cmd('colorscheme dracula')
-		end
-	}
+    -- Theme
+    use {
+        'maxmx03/dracula.nvim',
+        as = 'dracula',
+        config = function()
+            vim.cmd('colorscheme dracula')
+        end
+    }
 
-	-- Treesitter
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
+    -- Treesitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
 
-	-- Harpoon
-	use {'nvim-lua/plenary.nvim'}
-	use {'ThePrimeagen/harpoon'}
+    -- Harpoon
+    use {'nvim-lua/plenary.nvim'}
+    use {'ThePrimeagen/harpoon'}
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			--- Uncomment these if you want to manage the language servers from neovim
-			-- {'williamboman/mason.nvim'},
-			-- {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage the language servers from neovim
+            -- {'williamboman/mason.nvim'},
+            -- {'williamboman/mason-lspconfig.nvim'},
 
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
 
-	-- Mason
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	}
+    -- Mason
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
 
 end)
