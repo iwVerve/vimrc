56410ec5d184ceae73a59f4fc0a9466969486114
iwVerve <thedolphiner@gmail.com>
2024-01-08 21:27:34 +0100

    fix nvim-tree startup

--- lua/iwverve/init.lua
+++ lua/iwverve/init.lua
@@ -5,11 +5,13 @@ vim.g.loaded_netrwPlugin = 1
 require("iwverve.set")
 require("iwverve.remap")
 
-vim.cmd("vsplit")
-vim.cmd("wincmd l")
-vim.cmd("enew")
-vim.cmd("vsplit")
-vim.cmd("wincmd l")
-vim.cmd("enew")
-vim.cmd("wincmd h")
-vim.cmd("wincmd h")
+if table.getn(vim.v.argv) > 2 then
+    vim.cmd("vsplit")
+    vim.cmd("wincmd l")
+    vim.cmd("enew")
+    vim.cmd("wincmd h")
+    vim.cmd("vsplit")
+    vim.cmd("wincmd l")
+    vim.cmd("enew")
+    vim.cmd("wincmd h")
+end
