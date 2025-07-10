b84f5e53270271d679c725daf58ff6c2b0cae0db
iwVerve <thedolphiner@gmail.com>
2025-07-10 23:06:35 +0200

    typo

--- lua/iwverve/plugins/lsp.lua
+++ lua/iwverve/plugins/lsp.lua
@@ -16,9 +16,8 @@ return {
         vim.api.nvim_create_autocmd('LspAttach', {
             callback = function(event)
                 local opts = { buffer = event.buf }
-
                 vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-                vim.keymap.set('n', '<leader>vd', function() vim.lsp.diagnostic.open_float() end, opts)
+                vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
                 vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
                 vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
